59b4ccb6e829501fdc717595fd92068109e158db
musiccity365 <m_sackstein@yahoo.com>
2020-10-20 20:58:45 -0400

    --- Auto-Git Commit ---

--- lib/api.rb
+++ lib/api.rb
@@ -1,6 +1,9 @@
 class API
     # all APIs should be class methods
     def self.fetch_phones
-        
+        # url = ""
+        binding.pry
+        response = Net::HTTP.get(url)
+        hash = JSON.parse(response)
     end
 end
