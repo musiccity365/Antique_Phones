b0125fc76fb3ac1f01b3e5a60f3b8cab9cad28df
musiccity365 <m_sackstein@yahoo.com>
2020-10-21 19:30:05 -0400

    --- Auto-Git Commit ---

--- lib/api.rb
+++ lib/api.rb
@@ -1,10 +1,9 @@
 class API
     # all APIs should be class methods
     def self.fetch_movies
-        url = "https://www.themoviedb.org/documentation/api"
-
-        binding.pry
-        # response = Net::HTTP.get(url)
-        # hash = JSON.parse(response)
+        url = "https://ghibliapi.herokuapp.com/films"
+        uri = URI(url)
+        response = Net::HTTP.get(uri) # => String
+        hash = JSON.parse(response)
     end
 end
